46a873bdb9a7c4b2d3d02c686a097c292072f949
matts134 <mshopsin@gmail.com>
2013-04-27 23:32:00 -0700

    Document Page made and sign in styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BackendDeviseTest::Application.routes.draw do
   devise_for :users
 
-  root :to => "home#index"
+  root :to => "documents#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
