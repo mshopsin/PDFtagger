fc1094ac3fe57356df7942227abbb28720a67b6a
matts134 <mshopsin@gmail.com>
2013-05-01 16:35:10 -0700

    facebook login attempt 14 fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def after_sign_in_path_for(resource_or_scope)
-     if request.env['omniauth.origin']
-        request.env['omniauth.origin']
-     end
-  end
+  # def after_sign_in_path_for(resource_or_scope)
+  #    if request.env['omniauth.origin']
+  #       request.env['omniauth.origin']
+  #    end
+  # end
   
 end
