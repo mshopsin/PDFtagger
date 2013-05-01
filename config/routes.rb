fb1679823f3cc88b5cac7820f4bfff6d3a25afd8
matts134 <mshopsin@gmail.com>
2013-05-01 16:30:26 -0700

    facebook login attempt 13 reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 BackendDeviseTest::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
   root :to => "documents#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
   
   resources :documents
   resources :tags
