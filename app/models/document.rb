c308be210a83bb86078adecb2e01933c2398690d
matts134 <mshopsin@gmail.com>
2013-05-01 10:31:07 -0700

    removing paper clip

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :tags, dependent: :destroy
   has_many :posts, through: :tags
-  has_attached_file :pdf
+  #has_attached_file :pdf
   
   validates :title, presence: true
   validates :title, uniqueness: true
