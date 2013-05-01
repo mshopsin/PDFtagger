f221f2febfc5e3f12315fc98df98a1809be2c7ab
matts134 <mshopsin@gmail.com>
2013-05-01 15:41:30 -0700

    facebook login attempt 11 forced save of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
                            email:auth.info.email,
                            password:Devise.friendly_token[0,20]
                            )
+                           user.save
     end
     puts user.persisted?
     user
